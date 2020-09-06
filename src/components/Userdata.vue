df1ba4b7617c2204cbc39801763cb472dbbf4f97
galibshams24gss <galibshams24gss@gmail.com>
2020-09-06 14:08:29 +1000

    dev commit

--- src/components/Userdata.vue
+++ src/components/Userdata.vue
@@ -93,14 +93,14 @@
       <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
         Submit
       </button>
-      <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="/">
-        Cancel
-      </a>
-    </div>
-    <div class="flex justify-center">
       <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" @click="backToOriginal" type="submit">
         Revert
       </button>
+    </div>
+    <div class="flex justify-center">
+      <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="/">
+        Cancel
+      </a>
     </div>
   </form>
     </div>
